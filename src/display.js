8d669dd81a501e2a8b7c66b0b3cc1d10aa5f6779
michaelhtran120 <michaelhtran120@gmail.com>
2021-06-16 15:30:31 -0700

    updates

--- src/display.js
+++ src/display.js
@@ -1,24 +1,6 @@
 import { format, addDays } from "date-fns";
 
-let defaultList = [
-  {
-    title: "Ex. Groceries",
-    description: "Buy item 1, item 2, item 3",
-    dueDate: format(addDays(new Date(), 1), "yyyy-MM-dd"),
-    priority: "high",
-    isCompleted: false,
-    project: "ex.shopping",
-  },
-  {
-    title: "Ex .Workout",
-    description: "3 sets of bicep, 3 sets of tricep",
-    dueDate: format(new Date(), "yyyy-MM-dd"),
-    priority: "medium",
-    isCompleted: true,
-    project: "ex.fitness",
-  },
-];
-
+let defaultList = [];
 const displayTodo = (arr) => {
   arr.forEach((item) => {
     const todoContainer = document.querySelector("#todos-container");
@@ -300,5 +282,25 @@ function displayCustomProject(array) {
 
 if (localStorage.getItem("itemsStorage") !== null) {
   defaultList = JSON.parse(localStorage.getItem("itemsStorage"));
+} else {
+  defaultList = [
+    {
+      title: "Ex. Groceries",
+      description: "Buy item 1, item 2, item 3",
+      dueDate: format(addDays(new Date(), 1), "yyyy-MM-dd"),
+      priority: "high",
+      isCompleted: false,
+      project: "ex.shopping",
+    },
+    {
+      title: "Ex .Workout",
+      description: "3 sets of bicep, 3 sets of tricep",
+      dueDate: format(new Date(), "yyyy-MM-dd"),
+      priority: "medium",
+      isCompleted: true,
+      project: "ex.fitness",
+    },
+  ];
 }
+
 export { displayTodo, removeAllChildNodes, defaultList, displayCustomProject };
